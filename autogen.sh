a30588787a8cda4c049ed9f11e20d06e1029be8c
Hans Petter Jansson <hpj@cl.no>
2018-03-10 15:34:25 +0100

    build: Improve formatting of autogen script.

--- autogen.sh
+++ autogen.sh
@@ -15,40 +15,41 @@ MISSING_TOOLS=
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
         MISSING_TOOLS="${MISSING_TOOLS}autoconf "
-	DIE=1
+        DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
         MISSING_TOOLS="${MISSING_TOOLS}automake "
-	DIE=1
+        DIE=1
 }
 
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
         MISSING_TOOLS="${MISSING_TOOLS}libtool "
-	DIE=1
+        DIE=1
 }
 
 if test "$DIE" -eq 1; then
         echo
         echo "The following tools were not found: $MISSING_TOOLS"
+        echo
         echo "These are required for building Chafa from its git repository."
         echo "You should be able to install these using your operating system's"
         echo "package manager (apt-get, yum, zypper or similar). Alternately"
         echo "they can be obtained directly from GNU: http://ftp.gnu.org/gnu/"
         echo
-	exit 1
+        exit 1
 fi
 
 test $TEST_TYPE $FILE || {
         echo
-	echo "You must run this script in the top-level $PROJECT directory"
+        echo "You must run this script in the top-level $PROJECT directory"
         echo
-	exit 1
+        exit 1
 }
 
 if test -z "$*"; then
         echo
-	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "I am going to run ./configure with no arguments - if you wish "
         echo "to pass any to it, please specify them on the $0 command line."
         echo
 fi
