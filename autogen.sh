169c0779f200888ca198077fd34e77766bb061fc
Hans Petter Jansson <hpj@cl.no>
2018-03-10 15:33:24 +0100

    build: Have autogen check for libtool before proceeding. Improve output somewhat.

--- autogen.sh
+++ autogen.sh
@@ -8,37 +8,49 @@ ORIGDIR=`pwd`
 cd $srcdir
 PROJECT=chafa
 TEST_TYPE=-f
-FILE=chafa/Makefile.am  # Replace with project-specific file.
+FILE=chafa/Makefile.am
 DIE=0
 
+MISSING_TOOLS=
+
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+        MISSING_TOOLS="${MISSING_TOOLS}autoconf "
 	DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.4.tar.gz"
-	echo "(or a newer version if it is available)"
+        MISSING_TOOLS="${MISSING_TOOLS}automake "
+	DIE=1
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+        MISSING_TOOLS="${MISSING_TOOLS}libtool "
 	DIE=1
 }
 
 if test "$DIE" -eq 1; then
+        echo
+        echo "The following tools were not found: $MISSING_TOOLS"
+        echo "These are required for building Chafa from its git repository."
+        echo "You should be able to install these using your operating system's"
+        echo "package manager (apt-get, yum, zypper or similar). Alternately"
+        echo "they can be obtained directly from GNU: http://ftp.gnu.org/gnu/"
+        echo
 	exit 1
 fi
 
 test $TEST_TYPE $FILE || {
+        echo
 	echo "You must run this script in the top-level $PROJECT directory"
+        echo
 	exit 1
 }
 
 if test -z "$*"; then
+        echo
 	echo "I am going to run ./configure with no arguments - if you wish "
         echo "to pass any to it, please specify them on the $0 command line."
+        echo
 fi
 
 am_opt="--include-deps --add-missing"
