245d04be4f3ab86eb02477d671153c92e020eaf9
Hans Petter Jansson <hpj@cl.no>
2018-03-24 04:27:34 +0100

    build: Add checks required for gtk-doc and man pages.

--- autogen.sh
+++ autogen.sh
@@ -13,6 +13,18 @@ DIE=0
 
 MISSING_TOOLS=
 
+GTKDOCIZE=$(which gtkdocize 2>/dev/null)
+if test -z $GTKDOCIZE; then
+        echo "You don't have gtk-doc installed, and thus won't be able to generate the documentation."
+        rm -f gtk-doc.make
+        cat > gtk-doc.make <<EOF
+EXTRA_DIST =
+CLEANFILES =
+EOF
+else
+        gtkdocize || exit $?
+fi
+
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
         MISSING_TOOLS="${MISSING_TOOLS}autoconf "
         DIE=1
