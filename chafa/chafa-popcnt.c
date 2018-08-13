1398b7db5b10f0e4a870ca9febaaffc119258d59
Mo Zhou <cdluminate@gmail.com>
2018-08-13 05:59:01 +0000

    Compile 32bit popcnt code if _mm_popcnt_u32 is detected.
    Resulting code for i386 will use popcnt intrinsics if available, by default.

--- chafa/chafa-popcnt.c
+++ chafa/chafa-popcnt.c
@@ -26,9 +26,9 @@
 gint
 chafa_pop_count_u64_builtin (guint64 v)
 {
-#if !defined(CHAFA_REG_BIT32)
+#if defined(HAVE_POPCNT64_INTRINSICS)
     return (gint) _mm_popcnt_u64 (v);
-#else
+#else /* HAVE_POPCNT32_INTRINSICS */
     __int32_t* w = (__int32_t*)&v;
     return (gint) _mm_popcnt_u32(w[0]) + _mm_popcnt_u32(w[1]);
 #endif
@@ -39,9 +39,9 @@ chafa_pop_count_vu64_builtin (const guint64 *vv, gint *vc, gint n)
 {
     while (n--)
     {
-#if !defined(CHAFA_REG_BIT32)
+#if defined(HAVE_POPCNT64_INTRINSICS)
         *(vc++) = _mm_popcnt_u64 (*(vv++));
-#else
+#else /* HAVE_POPCNT32_INTRINSICS */
          __int32_t* w = (__int32_t*)vv;
         *(vc++) = _mm_popcnt_u32(w[0]) + _mm_popcnt_u32(w[1]);
         vv++;
@@ -52,7 +52,7 @@ chafa_pop_count_vu64_builtin (const guint64 *vv, gint *vc, gint n)
 void
 chafa_hamming_distance_vu64_builtin (guint64 a, const guint64 *vb, gint *vc, gint n)
 {
-#if !defined(CHAFA_REG_BIT32)
+#if defined(HAVE_POPCNT64_INTRINSICS)
     while (n >= 4)
     {
         n -= 4;
@@ -65,7 +65,7 @@ chafa_hamming_distance_vu64_builtin (guint64 a, const guint64 *vb, gint *vc, gin
     while (n--) {
         *(vc++) = _mm_popcnt_u64 (a ^ *(vb++));
     }
-#else
+#else /* HAVE_POPCNT32_INTRINSICS */
     __int32_t* aa = (__int32_t*)&a;
     __int32_t* wb = (__int32_t*)vb;
     while (n--) {
