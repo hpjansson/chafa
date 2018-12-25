404470ca9d34aa7aa691a3bf22680d2ec8bf0e8d
Hans Petter Jansson <hpj@cl.no>
2018-12-25 13:40:29 +0100

    libnsgif: Bundle libnsgif version 0.2.1.

--- chafa/chafa.c
+++ chafa/chafa.c
@@ -164,7 +164,8 @@ collect_variable_arguments (int *argc, char **argv [], gint first_arg)
 }
 
 static const gchar copyright_notice [] =
-    "Copyright (C) 2018 Hans Petter Jansson\n"
+    "Copyright (C) 2018 Hans Petter Jansson et al.\n"
+    "Incl. libnsgif copyright (C) 2004 Richard Wilson, copyright (C) 2008 Sean Fox\n\n"
     "This is free software; see the source for copying conditions.  There is NO\n"
     "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
 
