ca6662a5359e555e304f0b39fbbd134659b3e006
Hans Petter Jansson <hpj@cl.no>
2020-07-08 23:14:53 +0200

    libchafa: Generate docstrings for emitted sequences

---
+++ chafa/chafa-term-seq-doc-in.h
@@ -0,0 +1,22 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/* Copyright (C) 2020 Hans Petter Jansson
+ *
+ * This file is part of Chafa, a program that turns images into character art.
+ *
+ * Chafa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Chafa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Chafa.  If not, see <http://www.gnu.org/licenses/>. */
+
+#define CHAFA_TERM_SEQ_DEF(name, NAME, n_args, arg_type, ...) \
+    CHAFA_TERM_SEQ_AVAILABILITY gchar *chafa_term_info_emit_##name (const ChafaTermInfo *term_info, gchar *dest __VA_ARGS__);
+#include "chafa-term-seq-def.h"
