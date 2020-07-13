cbc22bfb02543726af0ce83f18044da676ea1752
Hans Petter Jansson <hpj@cl.no>
2020-07-13 22:29:07 +0200

    ChafaCanvas: Use ChafaTermInfo to emit control sequences

--- chafa/internal/chafa-canvas-printer.c
+++ chafa/internal/chafa-canvas-printer.c
@@ -22,8 +22,8 @@
 #include "chafa.h"
 #include "internal/chafa-canvas-printer.h"
 
-static void
-emit_ansi_truecolor (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
+static gchar *
+emit_ansi_truecolor (ChafaCanvas *canvas, ChafaTermInfo *ti, gchar *out, gint i, gint i_max)
 {
     for ( ; i < i_max; i++)
     {
@@ -41,37 +41,44 @@ emit_ansi_truecolor (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
         {
             if (bg.ch [3] < canvas->config.alpha_threshold)
             {
+                out = chafa_term_info_emit_reset_attributes (ti, out);
                 /* FIXME: Respect include/exclude for space */
+                *(out++) = ' ';
                 if (i < i_max - 1 && canvas->cells [i + 1].c == 0)
-                    g_string_append (gs, "\x1b[0m  ");
-                else
-                    g_string_append (gs, "\x1b[0m ");
+                    *(out++) = ' ';
             }
             else
             {
-                g_string_append_printf (gs, "\x1b[0m\x1b[7m\x1b[38;2;%d;%d;%dm",
-                                        bg.ch [0], bg.ch [1], bg.ch [2]);
-                g_string_append_unichar (gs, cell->c);
+                out = chafa_term_info_emit_reset_attributes (ti, out);
+                out = chafa_term_info_emit_invert_colors (ti, out);
+                out = chafa_term_info_emit_set_color_fg_direct (ti, out,
+                                                                bg.ch [0], bg.ch [1], bg.ch [2]);
+                out += g_unichar_to_utf8 (cell->c, out);
             }
         }
         else if (bg.ch [3] < canvas->config.alpha_threshold)
         {
-            g_string_append_printf (gs, "\x1b[0m\x1b[38;2;%d;%d;%dm",
-                                    fg.ch [0], fg.ch [1], fg.ch [2]);
-            g_string_append_unichar (gs, cell->c);
+            out = chafa_term_info_emit_reset_attributes (ti, out);
+            out = chafa_term_info_emit_set_color_fg_direct (ti, out,
+                                                            fg.ch [0], fg.ch [1], fg.ch [2]);
+            out += g_unichar_to_utf8 (cell->c, out);
         }
         else
         {
-            g_string_append_printf (gs, "\x1b[0m\x1b[38;2;%d;%d;%dm\x1b[48;2;%d;%d;%dm",
-                                    fg.ch [0], fg.ch [1], fg.ch [2],
-                                    bg.ch [0], bg.ch [1], bg.ch [2]);
-            g_string_append_unichar (gs, cell->c);
+            out = chafa_term_info_emit_reset_attributes (ti, out);
+            out = chafa_term_info_emit_set_color_fg_direct (ti, out,
+                                                            fg.ch [0], fg.ch [1], fg.ch [2]);
+            out = chafa_term_info_emit_set_color_bg_direct (ti, out,
+                                                            bg.ch [0], bg.ch [1], bg.ch [2]);
+            out += g_unichar_to_utf8 (cell->c, out);
         }
     }
+
+    return out;
 }
 
-static void
-emit_ansi_256 (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
+static gchar *
+emit_ansi_256 (ChafaCanvas *canvas, ChafaTermInfo *ti, gchar *out, gint i, gint i_max)
 {
     for ( ; i < i_max; i++)
     {
@@ -85,37 +92,41 @@ emit_ansi_256 (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
         {
             if (cell->bg_color == CHAFA_PALETTE_INDEX_TRANSPARENT)
             {
+                out = chafa_term_info_emit_reset_attributes (ti, out);
+                /* FIXME: Respect include/exclude for space */
+                *(out++) = ' ';
                 if (i < i_max - 1 && canvas->cells [i + 1].c == 0)
-                    g_string_append (gs, "\x1b[0m  ");
-                else
-                    g_string_append (gs, "\x1b[0m ");
+                    *(out++) = ' ';
             }
             else
             {
-                g_string_append_printf (gs, "\x1b[0m\x1b[7m\x1b[38;5;%dm",
-                                        cell->bg_color);
-                g_string_append_unichar (gs, cell->c);
+                out = chafa_term_info_emit_reset_attributes (ti, out);
+                out = chafa_term_info_emit_invert_colors (ti, out);
+                out = chafa_term_info_emit_set_color_fg_256 (ti, out, cell->bg_color);
+                out += g_unichar_to_utf8 (cell->c, out);
             }
         }
         else if (cell->bg_color == CHAFA_PALETTE_INDEX_TRANSPARENT)
         {
-            g_string_append_printf (gs, "\x1b[0m\x1b[38;5;%dm",
-                                    cell->fg_color);
-            g_string_append_unichar (gs, cell->c);
+            out = chafa_term_info_emit_reset_attributes (ti, out);
+            out = chafa_term_info_emit_set_color_fg_256 (ti, out, cell->fg_color);
+            out += g_unichar_to_utf8 (cell->c, out);
         }
         else
         {
-            g_string_append_printf (gs, "\x1b[0m\x1b[38;5;%dm\x1b[48;5;%dm",
-                                    cell->fg_color,
-                                    cell->bg_color);
-            g_string_append_unichar (gs, cell->c);
+            out = chafa_term_info_emit_reset_attributes (ti, out);
+            out = chafa_term_info_emit_set_color_fg_256 (ti, out, cell->fg_color);
+            out = chafa_term_info_emit_set_color_bg_256 (ti, out, cell->bg_color);
+            out += g_unichar_to_utf8 (cell->c, out);
         }
     }
+
+    return out;
 }
 
 /* Uses aixterm control codes for bright colors */
-static void
-emit_ansi_16 (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
+static gchar *
+emit_ansi_16 (ChafaCanvas *canvas, ChafaTermInfo *ti, gchar *out, gint i, gint i_max)
 {
     for ( ; i < i_max; i++)
     {
@@ -129,40 +140,40 @@ emit_ansi_16 (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
         {
             if (cell->bg_color == CHAFA_PALETTE_INDEX_TRANSPARENT)
             {
+                out = chafa_term_info_emit_reset_attributes (ti, out);
+                /* FIXME: Respect include/exclude for space */
+                *(out++) = ' ';
                 if (i < i_max - 1 && canvas->cells [i + 1].c == 0)
-                    g_string_append (gs, "\x1b[0m  ");
-                else
-                    g_string_append (gs, "\x1b[0m ");
+                    *(out++) = ' ';
             }
             else
             {
-                g_string_append_printf (gs, "\x1b[0m\x1b[7m\x1b[%dm",
-                                        cell->bg_color < 8 ? cell->bg_color + 30
-                                                           : cell->bg_color + 90 - 8);
-                g_string_append_unichar (gs, cell->c);
+                out = chafa_term_info_emit_reset_attributes (ti, out);
+                out = chafa_term_info_emit_invert_colors (ti, out);
+                out = chafa_term_info_emit_set_color_fg_16 (ti, out, cell->bg_color);
+                out += g_unichar_to_utf8 (cell->c, out);
             }
         }
         else if (cell->bg_color == CHAFA_PALETTE_INDEX_TRANSPARENT)
         {
-            g_string_append_printf (gs, "\x1b[0m\x1b[%dm",
-                                    cell->fg_color < 8 ? cell->fg_color + 30
-                                                       : cell->fg_color + 90 - 8);
-            g_string_append_unichar (gs, cell->c);
+            out = chafa_term_info_emit_reset_attributes (ti, out);
+            out = chafa_term_info_emit_set_color_fg_16 (ti, out, cell->fg_color);
+            out += g_unichar_to_utf8 (cell->c, out);
         }
         else
         {
-            g_string_append_printf (gs, "\x1b[0m\x1b[%dm\x1b[%dm",
-                                    cell->fg_color < 8 ? cell->fg_color + 30
-                                                       : cell->fg_color + 90 - 8,
-                                    cell->bg_color < 8 ? cell->bg_color + 40
-                                                       : cell->bg_color + 100 - 8);
-            g_string_append_unichar (gs, cell->c);
+            out = chafa_term_info_emit_reset_attributes (ti, out);
+            out = chafa_term_info_emit_set_color_fg_256 (ti, out, cell->fg_color);
+            out = chafa_term_info_emit_set_color_bg_256 (ti, out, cell->bg_color);
+            out += g_unichar_to_utf8 (cell->c, out);
         }
     }
+
+    return out;
 }
 
-static void
-emit_ansi_fgbg_bgfg (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
+static gchar *
+emit_ansi_fgbg_bgfg (ChafaCanvas *canvas, ChafaTermInfo *ti, gchar *out, gint i, gint i_max)
 {
     gunichar blank_symbol = 0;
 
@@ -199,14 +210,19 @@ emit_ansi_fgbg_bgfg (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
             invert ^= TRUE;
         }
 
-        g_string_append_printf (gs, "\x1b[%dm",
-                                invert ? 7 : 0);
-        g_string_append_unichar (gs, c);
+        if (invert)
+            out = chafa_term_info_emit_invert_colors (ti, out);
+        else
+            out = chafa_term_info_emit_reset_attributes (ti, out);
+
+        out += g_unichar_to_utf8 (cell->c, out);
     }
+
+    return out;
 }
 
-static void
-emit_ansi_fgbg (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
+static gchar *
+emit_ansi_fgbg (ChafaCanvas *canvas, G_GNUC_UNUSED ChafaTermInfo *ti, gchar *out, gint i, gint i_max)
 {
     for ( ; i < i_max; i++)
     {
@@ -216,12 +232,31 @@ emit_ansi_fgbg (ChafaCanvas *canvas, GString *gs, gint i, gint i_max)
         if (cell->c == 0)
             continue;
 
-        g_string_append_unichar (gs, cell->c);
+        out += g_unichar_to_utf8 (cell->c, out);
+    }
+
+    return out;
+}
+
+static void
+prealloc_string (GString *gs, gint n_cells)
+{
+    guint needed_len;
+
+    /* Each cell produces at most three control sequences and six bytes
+     * for the UTF-8 character. Each row may add one seq and one newline. */
+    needed_len = (n_cells + 1) * (CHAFA_TERM_SEQ_LENGTH_MAX * 3 + 6) + 1;
+
+    if (gs->allocated_len - gs->len < needed_len)
+    {
+        guint current_len = gs->len;
+        g_string_set_size (gs, gs->len + needed_len * 2);
+        gs->len = current_len;
     }
 }
 
 static GString *
-build_ansi_gstring (ChafaCanvas *canvas)
+build_ansi_gstring (ChafaCanvas *canvas, ChafaTermInfo *ti)
 {
     GString *gs = g_string_new ("");
     gint i, i_max, i_step, i_next;
@@ -232,25 +267,30 @@ build_ansi_gstring (ChafaCanvas *canvas)
 
     for ( ; i < i_max; i = i_next)
     {
+        gchar *out;
+
         i_next = i + i_step;
+
+        prealloc_string (gs, i_step);
+        out = gs->str + gs->len;
 
         switch (canvas->config.canvas_mode)
         {
             case CHAFA_CANVAS_MODE_TRUECOLOR:
-                emit_ansi_truecolor (canvas, gs, i, i_next);
+                out = emit_ansi_truecolor (canvas, ti, out, i, i_next);
                 break;
             case CHAFA_CANVAS_MODE_INDEXED_256:
             case CHAFA_CANVAS_MODE_INDEXED_240:
-                emit_ansi_256 (canvas, gs, i, i_next);
+                out = emit_ansi_256 (canvas, ti, out, i, i_next);
                 break;
             case CHAFA_CANVAS_MODE_INDEXED_16:
-                emit_ansi_16 (canvas, gs, i, i_next);
+                out = emit_ansi_16 (canvas, ti, out, i, i_next);
                 break;
             case CHAFA_CANVAS_MODE_FGBG_BGFG:
-                emit_ansi_fgbg_bgfg (canvas, gs, i, i_next);
+                out = emit_ansi_fgbg_bgfg (canvas, ti, out, i, i_next);
                 break;
             case CHAFA_CANVAS_MODE_FGBG:
-                emit_ansi_fgbg (canvas, gs, i, i_next);
+                out = emit_ansi_fgbg (canvas, ti, out, i, i_next);
                 break;
             case CHAFA_CANVAS_MODE_MAX:
                 g_assert_not_reached ();
@@ -259,11 +299,14 @@ build_ansi_gstring (ChafaCanvas *canvas)
 
         /* No control codes in FGBG mode */
         if (canvas->config.canvas_mode != CHAFA_CANVAS_MODE_FGBG)
-            g_string_append (gs, "\x1b[0m");
+            out = chafa_term_info_emit_reset_attributes (ti, out);
 
         /* Last line should not end in newline */
         if (i_next < i_max)
-            g_string_append_c (gs, '\n');
+            *(out++) = '\n';
+
+        *out = '\0';
+        gs->len = out - gs->str;
     }
 
     return gs;
@@ -272,5 +315,22 @@ build_ansi_gstring (ChafaCanvas *canvas)
 GString *
 chafa_canvas_print (ChafaCanvas *canvas, ChafaTermInfo *ti)
 {
-    return build_ansi_gstring (canvas);
+    GString *str;
+
+    if (ti)
+    {
+        chafa_term_info_ref (ti);
+    }
+    else
+    {
+        gchar *env_vars [] = { "TERM=xterm-256color", NULL };
+        ChafaTermDb *term_db = chafa_term_db_new ();
+        ti = chafa_term_db_detect (term_db, env_vars);
+        chafa_term_db_unref (term_db);
+    }
+
+    str = build_ansi_gstring (canvas, ti);
+
+    chafa_term_info_unref (ti);
+    return str;
 }
