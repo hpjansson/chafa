603427b7a72121419bf6bbb66607893f2011a5b3
Hans Petter Jansson <hpj@hpjansson.org>
2021-01-11 01:15:55 +0100

    Update copyright blurbs for 2021

--- tools/chafa/chafa.c
+++ tools/chafa/chafa.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
-/* Copyright (C) 2018-2020 Hans Petter Jansson
+/* Copyright (C) 2018-2021 Hans Petter Jansson
  *
  * This file is part of Chafa, a program that turns images into character art.
  *
@@ -190,7 +190,7 @@ collect_variable_arguments (int *argc, char **argv [], gint first_arg)
 }
 
 static const gchar copyright_notice [] =
-    "Copyright (C) 2018-2020 Hans Petter Jansson et al.\n"
+    "Copyright (C) 2018-2021 Hans Petter Jansson et al.\n"
     "Incl. libnsgif copyright (C) 2004 Richard Wilson, copyright (C) 2008 Sean Fox\n\n"
     "This is free software; see the source for copying conditions.  There is NO\n"
     "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
