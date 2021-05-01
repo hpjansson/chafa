hpjansson/chafa

commit 44c93be7bcde7c346f6c22895f16d04219266c6f

symbols: Add Japanese kana symbols
This is just a rough import, the list will have to be pruned and some
of the outlines corrected.

chafa/internal/chafa-symbols.c
@@ -145,6 +145,7 @@ static const ChafaSymbolDef symbol_defs [] =
 {
 #include "chafa-symbols-ascii.h"
 #include "chafa-symbols-block.h"
+#include "chafa-symbols-kana.h"
 #include "chafa-symbols-misc-narrow.h"
     {
 #if CHAFA_USE_CONSTANT_STRING_EXPR

chafa/internal/Makefile.am
@@ -39,6 +39,7 @@ libchafa_internal_la_SOURCES = \
 	chafa-symbols.c \
 	chafa-symbols-ascii.h \
 	chafa-symbols-block.h \
+	chafa-symbols-kana.h \
 	chafa-symbols-misc-narrow.h \
 	chafa-work-cell.c \
 	chafa-work-cell.h

commit e56e6daad92b13cd80ec6c6d6a04de35ced08cd9

symbols: Fix bug introduced by kana symbols
Fix bug introduced in 44c93be7bcde7c346f6c22895f16d04219266c6f:

We weren't properly accounting for skipped-over wide symbols when
generating braille at the end of the narrow symbol array.

chafa/internal/chafa-symbols.c
@@ -369,7 +369,7 @@ init_symbol_array (const ChafaSymbolDef *defs)
         j++;
     }
 
-    generate_braille_syms (syms, i);
+    generate_braille_syms (syms, j);
     return syms;
 }
 