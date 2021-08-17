hpjansson/chafa

commit c5ce9708cc2f9069cc6df7007b177464377ef0e7

chafa, ChafaCanvas, ChafaCanvasConfig: Implement --hold-bg option
With this enabled, Chafa leaves the background color untouched. It will
produce character-cell output using foreground colors only, and avoids
resetting or inverting the colors.

chafa/internal/chafa-canvas-printer.c
@@ -129,9 +129,10 @@ emit_attributes_truecolor (PrintCtx *ctx, gchar *out,
 {
     if (ctx->canvas->config.optimizations & CHAFA_OPTIMIZATION_REUSE_ATTRIBUTES)
     {
-        if ((ctx->cur_inverted && !inverted)
-            || (ctx->cur_fg_direct.ch [3] != 0 && fg.ch [3] == 0)
-            || (ctx->cur_bg_direct.ch [3] != 0 && bg.ch [3] == 0))
+        if (!ctx->canvas->config.hold_bg
+            && ((ctx->cur_inverted && !inverted)
+                || (ctx->cur_fg_direct.ch [3] != 0 && fg.ch [3] == 0)
+                || (ctx->cur_bg_direct.ch [3] != 0 && bg.ch [3] == 0)))
         {
             out = flush_chars (ctx, out);
             out = reset_attributes (ctx, out);
@@ -238,24 +239,36 @@ emit_ansi_truecolor (PrintCtx *ctx, gchar *out, gint i, gint i_max)
 }
 
 static gchar *
+handle_inverted_with_reuse (PrintCtx *ctx, gchar *out,
+                            guint32 fg, guint32 bg, gboolean inverted)
+{
+    /* We must check hold_bg because we can run into the situation where fg is set
+     * to transparent. */
+    if (!ctx->canvas->config.hold_bg
+        && ((ctx->cur_inverted && !inverted)
+            || (ctx->cur_fg != CHAFA_PALETTE_INDEX_TRANSPARENT && fg == CHAFA_PALETTE_INDEX_TRANSPARENT)
+            || (ctx->cur_bg != CHAFA_PALETTE_INDEX_TRANSPARENT && bg == CHAFA_PALETTE_INDEX_TRANSPARENT)))
+    {
+        out = flush_chars (ctx, out);
+        out = reset_attributes (ctx, out);
+    }
+
+    if (!ctx->cur_inverted && inverted)
+    {
+        out = flush_chars (ctx, out);
+        out = chafa_term_info_emit_invert_colors (ctx->term_info, out);
+    }
+
+    return out;
+}
+
+static gchar *
 emit_attributes_256 (PrintCtx *ctx, gchar *out,
                      guint32 fg, guint32 bg, gboolean inverted)
 {
     if (ctx->canvas->config.optimizations & CHAFA_OPTIMIZATION_REUSE_ATTRIBUTES)
     {
-        if ((ctx->cur_inverted && !inverted)
-            || (ctx->cur_fg != CHAFA_PALETTE_INDEX_TRANSPARENT && fg == CHAFA_PALETTE_INDEX_TRANSPARENT)
-            || (ctx->cur_bg != CHAFA_PALETTE_INDEX_TRANSPARENT && bg == CHAFA_PALETTE_INDEX_TRANSPARENT))
-        {
-            out = flush_chars (ctx, out);
-            out = reset_attributes (ctx, out);
-        }
-
-        if (!ctx->cur_inverted && inverted)
-        {
-            out = flush_chars (ctx, out);
-            out = chafa_term_info_emit_invert_colors (ctx->term_info, out);
-        }
+        handle_inverted_with_reuse (ctx, out, fg, bg, inverted);
 
         if (fg != ctx->cur_fg)
         {
@@ -347,19 +360,7 @@ emit_attributes_16 (PrintCtx *ctx, gchar *out,
 {
     if (ctx->canvas->config.optimizations & CHAFA_OPTIMIZATION_REUSE_ATTRIBUTES)
     {
-        if ((ctx->cur_inverted && !inverted)
-            || (ctx->cur_fg != CHAFA_PALETTE_INDEX_TRANSPARENT && fg == CHAFA_PALETTE_INDEX_TRANSPARENT)
-            || (ctx->cur_bg != CHAFA_PALETTE_INDEX_TRANSPARENT && bg == CHAFA_PALETTE_INDEX_TRANSPARENT))
-        {
-            out = flush_chars (ctx, out);
-            out = reset_attributes (ctx, out);
-        }
-
-        if (!ctx->cur_inverted && inverted)
-        {
-            out = flush_chars (ctx, out);
-            out = chafa_term_info_emit_invert_colors (ctx->term_info, out);
-        }
+        handle_inverted_with_reuse (ctx, out, fg, bg, inverted);
 
         if (fg != ctx->cur_fg)
         {
@@ -571,9 +572,14 @@ build_ansi_gstring (ChafaCanvas *canvas, ChafaTermInfo *ti)
         prealloc_string (gs, i_step);
         out = gs->str + gs->len;
 
-        /* Avoid control codes in FGBG mode */
-        if (i == 0 && canvas->config.canvas_mode != CHAFA_CANVAS_MODE_FGBG)
+        /* Avoid control codes in FGBG mode. Don't reset attributes when BG
+         * is held, to preserve any BG color set previously. */
+        if (i == 0
+            && canvas->config.canvas_mode != CHAFA_CANVAS_MODE_FGBG
+            && !canvas->config.hold_bg)
+        {
             out = reset_attributes (&ctx, out);
+        }
 
         switch (canvas->config.canvas_mode)
         {
@@ -603,9 +609,13 @@ build_ansi_gstring (ChafaCanvas *canvas, ChafaTermInfo *ti)
 
         out = flush_chars (&ctx, out);
 
-        /* Avoid control codes in FGBG mode */
-        if (canvas->config.canvas_mode != CHAFA_CANVAS_MODE_FGBG)
+        /* Avoid control codes in FGBG mode. Don't reset attributes when BG
+         * is held, to preserve any BG color set previously. */
+        if (canvas->config.canvas_mode != CHAFA_CANVAS_MODE_FGBG
+            && !canvas->config.hold_bg)
+        {
             out = reset_attributes (&ctx, out);
+        }
 
         /* Last line should not end in newline */
         if (i_next < i_max)

chafa/chafa-canvas-config.c
@@ -117,6 +117,7 @@ chafa_canvas_config_init (ChafaCanvasConfig *canvas_config)
     canvas_config->work_factor = 0.5;
     canvas_config->preprocessing_enabled = TRUE;
     canvas_config->optimizations = CHAFA_OPTIMIZATION_ALL;
+    canvas_config->hold_bg = FALSE;
 
     chafa_symbol_map_init (&canvas_config->symbol_map);
     chafa_symbol_map_add_by_tags (&canvas_config->symbol_map, CHAFA_SYMBOL_TAG_BLOCK);
@@ -885,4 +886,51 @@ chafa_canvas_config_set_optimizations (ChafaCanvasConfig *config, ChafaOptimizat
     g_return_if_fail (config->refs > 0);
 
     config->optimizations = optimizations;
+}
+
+/**
+ * chafa_canvas_config_get_hold_bg:
+ * @config: A #ChafaCanvasConfig
+ *
+ * Queries whether the background color should be "held", or left unmodified,
+ * in the canvas output. This is relevant only when the #ChafaPixelMode is set
+ * to #CHAFA_PIXEL_MODE_SYMBOLS.
+ *
+ * When this is set, the canvas will emit escape codes to set the foreground
+ * color only.
+ *
+ * Returns: %TRUE if the background color will be held, %FALSE otherwise.
+ *
+ * Since: 1.8
+ **/
+gboolean
+chafa_canvas_config_get_hold_bg (const ChafaCanvasConfig *config)
+{
+    g_return_val_if_fail (config != NULL, CHAFA_OPTIMIZATION_NONE);
+    g_return_val_if_fail (config->refs > 0, CHAFA_OPTIMIZATION_NONE);
+
+    return config->hold_bg;
+}
+
+/**
+ * chafa_canvas_config_set_hold_bg:
+ * @config: A #ChafaCanvasConfig
+ * @hold_bg: Whether the background color should be held
+ *
+ * Indicates whether the background color should be "held", or left unmodified,
+ * in the canvas output. This is relevant only when the #ChafaPixelMode is set
+ * to #CHAFA_PIXEL_MODE_SYMBOLS.
+ *
+ * When this is set, the canvas will emit escape codes to set the foreground
+ * color only.
+ *
+ * Since: 1.8
+ **/
+void
+chafa_canvas_config_set_hold_bg (ChafaCanvasConfig *config, gboolean hold_bg)
+{
+    g_return_if_fail (config != NULL);
+    g_return_if_fail (config->refs > 0);
+
+    config->hold_bg = hold_bg;
 }

chafa/chafa-canvas-config.h
@@ -202,6 +202,11 @@ ChafaOptimizations chafa_canvas_config_get_optimizations (const ChafaCanvasConfi
 CHAFA_AVAILABLE_IN_1_6
 void chafa_canvas_config_set_optimizations (ChafaCanvasConfig *config, ChafaOptimizations optimizations);
 
+CHAFA_AVAILABLE_IN_1_8
+gboolean chafa_canvas_config_get_hold_bg (const ChafaCanvasConfig *config);
+CHAFA_AVAILABLE_IN_1_8
+void chafa_canvas_config_set_hold_bg (ChafaCanvasConfig *config, gboolean hold_bg);
+
 G_END_DECLS
 
 #endif /* __CHAFA_CANVAS_CONFIG_H__ */

chafa/chafa-canvas.c
@@ -146,6 +146,20 @@ packed_rgb_to_index (const ChafaPalette *palette, ChafaColorSpace cs, gint rgb)
     return ccand.index [0];
 }
 
+static guint32
+transparent_cell_color (ChafaCanvasMode canvas_mode)
+{
+    if (canvas_mode == CHAFA_CANVAS_MODE_TRUECOLOR)
+    {
+        const ChafaColor col = { { 0x80, 0x80, 0x80, 0x00 } };
+        return chafa_pack_color (&col);
+    }
+    else
+    {
+        return CHAFA_PALETTE_INDEX_TRANSPARENT;
+    }
+}
+
 static void
 eval_symbol_colors (ChafaCanvas *canvas, ChafaWorkCell *wcell, const ChafaSymbol *sym, SymbolEval *eval)
 {
@@ -256,7 +270,8 @@ eval_symbol (ChafaCanvas *canvas, ChafaWorkCell *wcell, gint sym_index,
 
     sym = &canvas->config.symbol_map.symbols [sym_index];
 
-    if (canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG)
+    if (canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG
+        || canvas->config.hold_bg)
     {
         eval.colors = canvas->default_colors;
     }
@@ -283,7 +298,8 @@ eval_symbol_wide (ChafaCanvas *canvas, ChafaWorkCell *wcell_a, ChafaWorkCell *wc
 
     sym2 = &canvas->config.symbol_map.symbols2 [sym_index];
 
-    if (canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG)
+    if (canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG
+        || canvas->config.hold_bg)
     {
         eval.colors = canvas->default_colors;
     }
@@ -330,6 +346,9 @@ pick_symbol_and_colors_slow (ChafaCanvas *canvas,
 
     g_assert (best_symbol >= 0);
 
+    if (canvas->config.hold_bg)
+        eval_symbol_colors (canvas, wcell, &canvas->config.symbol_map.symbols [best_symbol], &best_eval);
+
     *sym_out = canvas->config.symbol_map.symbols [best_symbol].c;
     *color_pair_out = best_eval.colors;
 
@@ -363,6 +382,12 @@ pick_symbol_and_colors_wide_slow (ChafaCanvas *canvas,
 
     g_assert (best_symbol >= 0);
 
+    if (canvas->config.hold_bg)
+        eval_symbol_colors_wide (canvas, wcell_a, wcell_b,
+                                 &canvas->config.symbol_map.symbols2 [best_symbol].sym [0],
+                                 &canvas->config.symbol_map.symbols2 [best_symbol].sym [1],
+                                 &best_eval);
+
     *sym_out = canvas->config.symbol_map.symbols2 [best_symbol].sym [0].c;
     *color_pair_out = best_eval.colors;
 
@@ -390,7 +415,8 @@ pick_symbol_and_colors_fast (ChafaCanvas *canvas,
     /* Generate short list of candidates */
 
     if (canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG
-        || canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG_BGFG)
+        || canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG_BGFG
+        || canvas->config.hold_bg)
     {
         color_pair = canvas->default_colors;
     }
@@ -405,6 +431,7 @@ pick_symbol_and_colors_fast (ChafaCanvas *canvas,
     chafa_symbol_map_find_candidates (&canvas->config.symbol_map,
                                       bitmap,
                                       canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG
+                                      || canvas->config.hold_bg
                                       ? FALSE : TRUE,  /* Consider inverted? */
                                       candidates, &n_candidates);
 
@@ -423,6 +450,9 @@ pick_symbol_and_colors_fast (ChafaCanvas *canvas,
     /* Output */
 
     g_assert (best_symbol >= 0);
+
+    if (canvas->config.hold_bg)
+        eval_symbol_colors (canvas, wcell, &canvas->config.symbol_map.symbols [best_symbol], &best_eval);
 
     *sym_out = canvas->config.symbol_map.symbols [best_symbol].c;
     *color_pair_out = best_eval.colors;
@@ -473,6 +503,7 @@ pick_symbol_and_colors_wide_fast (ChafaCanvas *canvas,
     chafa_symbol_map_find_wide_candidates (&canvas->config.symbol_map,
                                            bitmaps,
                                            canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG
+                                           || canvas->config.hold_bg
                                            ? FALSE : TRUE,  /* Consider inverted? */
                                            candidates, &n_candidates);
 
@@ -492,6 +523,12 @@ pick_symbol_and_colors_wide_fast (ChafaCanvas *canvas,
     /* Output */
 
     g_assert (best_symbol >= 0);
+
+    if (canvas->config.hold_bg)
+        eval_symbol_colors_wide (canvas, wcell_a, wcell_b,
+                                 &canvas->config.symbol_map.symbols2 [best_symbol].sym [0],
+                                 &canvas->config.symbol_map.symbols2 [best_symbol].sym [1],
+                                 &best_eval);
 
     *sym_out = canvas->config.symbol_map.symbols2 [best_symbol].sym [0].c;
     *color_pair_out = best_eval.colors;
@@ -519,6 +556,58 @@ static const ChafaColor *
 get_palette_color (ChafaCanvas *canvas, gint index)
 {
     return get_palette_color_with_color_space (canvas, index, canvas->config.color_space);
+}
+
+static void
+apply_fill_hold_bg (ChafaCanvas *canvas, const ChafaWorkCell *wcell, ChafaCanvasCell *cell)
+{
+    ChafaColor mean;
+    ChafaColorCandidates ccand;
+    gint fg_value, bg_value, mean_value;
+    gint n_bits;
+    ChafaCandidate sym_cand;
+    gint n_sym_cands = 1;
+
+    if (canvas->config.fill_symbol_map.n_symbols == 0)
+        return;
+
+    chafa_work_cell_calc_mean_color (wcell, &mean);
+
+    if (canvas->config.canvas_mode == CHAFA_CANVAS_MODE_TRUECOLOR)
+    {
+        cell->fg_color = chafa_pack_color (&mean);
+    }
+    else
+    {
+        chafa_palette_lookup_nearest (&canvas->palette, canvas->config.color_space, &mean, &ccand);
+        cell->fg_color = ccand.index [0];
+    }
+
+    cell->bg_color = transparent_cell_color (canvas->config.canvas_mode);
+
+    /* FIXME: Do we care enough to weight channels properly here, or convert from DIN99d?
+     * Output looks acceptable without. Would have to check if it makes a noticeable
+     * difference. */
+    fg_value = (canvas->default_colors.colors [CHAFA_COLOR_PAIR_FG].ch [0]
+                + canvas->default_colors.colors [CHAFA_COLOR_PAIR_FG].ch [1]
+                + canvas->default_colors.colors [CHAFA_COLOR_PAIR_FG].ch [2])
+               / 3;
+    bg_value = (canvas->default_colors.colors [CHAFA_COLOR_PAIR_BG].ch [0]
+                + canvas->default_colors.colors [CHAFA_COLOR_PAIR_BG].ch [1]
+                + canvas->default_colors.colors [CHAFA_COLOR_PAIR_BG].ch [2])
+               / 3;
+    mean_value = (mean.ch [0] + mean.ch [1] + mean.ch [2]) / 3;
+
+    n_bits = ((mean_value * 64) + 128) / 255;
+    if (fg_value < bg_value)
+        n_bits = 64 - n_bits;
+
+    chafa_symbol_map_find_fill_candidates (&canvas->config.fill_symbol_map,
+                                           n_bits,
+                                           FALSE,
+                                           &sym_cand, &n_sym_cands);
+
+    cell->c = canvas->config.fill_symbol_map.symbols [sym_cand.symbol_index].c;
 }
 
 static void
@@ -595,6 +684,7 @@ apply_fill (ChafaCanvas *canvas, const ChafaWorkCell *wcell, ChafaCanvasCell *ce
 
     chafa_symbol_map_find_fill_candidates (&canvas->config.fill_symbol_map, best_i,
                                            canvas->config.canvas_mode == CHAFA_CANVAS_MODE_FGBG
+                                           || canvas->config.hold_bg
                                            ? FALSE : TRUE,  /* Consider inverted? */
                                            &sym_cand, &n_sym_cands);
 
@@ -659,6 +749,9 @@ update_cell (ChafaCanvas *canvas, ChafaWorkCell *work_cell, ChafaCanvasCell *cel
         cell_out->bg_color = chafa_pack_color (&color_pair.colors [CHAFA_COLOR_PAIR_BG]);
     }
 
+    if (canvas->config.hold_bg)
+        cell_out->bg_color = transparent_cell_color (canvas->config.canvas_mode);
+
     /* FIXME: It would probably be better to do the fgbg/bgfg blank symbol check
      * from emit_ansi_fgbg_bgfg() here. */
 
@@ -707,6 +800,9 @@ update_cells_wide (ChafaCanvas *canvas, ChafaWorkCell *work_cell_a, ChafaWorkCel
         cell_a_out->fg_color = cell_b_out->fg_color = chafa_pack_color (&color_pair.colors [CHAFA_COLOR_PAIR_FG]);
         cell_a_out->bg_color = cell_b_out->bg_color = chafa_pack_color (&color_pair.colors [CHAFA_COLOR_PAIR_BG]);
     }
+
+    if (canvas->config.hold_bg)
+        cell_a_out->bg_color = cell_b_out->bg_color = transparent_cell_color (canvas->config.canvas_mode);
 }
 
 /* Number of entries in our cell ring buffer. This allows us to do lookback
@@ -778,7 +874,15 @@ update_cells_row (ChafaCanvas *canvas, gint row)
         if (cells [cx].c != 0 && (cells [cx].c == ' ' || cells [cx].c == 0x2588
                                   || cells [cx].fg_color == cells [cx].bg_color))
         {
-            apply_fill (canvas, wcell, &cells [cx]);
+            if (canvas->config.hold_bg)
+            {
+                apply_fill_hold_bg (canvas, wcell, &cells [cx]);
+                cells [cx].bg_color = transparent_cell_color (canvas->config.canvas_mode);
+            }
+            else
+            {
+                apply_fill (canvas, wcell, &cells [cx]);
+            }
         }
 
         /* If cell is still featureless after fill, use blank_char consistently */
@@ -832,6 +936,19 @@ update_cells (ChafaCanvas *canvas)
 }
 
 static void
+differentiate_channel (guint8 *dest_channel, guint8 reference_channel, gint min_diff)
+{
+    gint diff;
+
+    diff = (gint) *dest_channel - (gint) reference_channel;
+
+    if (diff >= -min_diff && diff <= 0)
+        *dest_channel = MAX ((gint) reference_channel - min_diff, 0);
+    else if (diff <= min_diff && diff >= 0)
+        *dest_channel = MIN ((gint) reference_channel + min_diff, 255);
+}
+
+static void
 update_display_colors (ChafaCanvas *canvas)
 {
     ChafaColor fg_col;
@@ -853,6 +970,34 @@ update_display_colors (ChafaCanvas *canvas)
 
     canvas->default_colors.colors [CHAFA_COLOR_PAIR_FG].ch [3] = 0xff;
     canvas->default_colors.colors [CHAFA_COLOR_PAIR_BG].ch [3] = 0x00;
+
+    /* When holding the BG, we need to compare against a consistent
+     * foreground color for symbol selection by outline. 50% gray
+     * yields acceptable results as a stand-in average of all possible
+     * colors. The BG color can't be too similar, so push it away a
+     * little if needed. This should work with both bright and dark
+     * background colors, and the background color doesn't have to
+     * be precise.
+     *
+     * We don't need to do this for monochrome modes, as they use the
+     * FG/BG colors directly. */
+
+    if (canvas->config.hold_bg
+        && canvas->config.canvas_mode != CHAFA_CANVAS_MODE_FGBG
+        && canvas->config.canvas_mode != CHAFA_CANVAS_MODE_FGBG_BGFG)
+    {
+        gint i;
+
+        chafa_unpack_color (0xff7f7f7f,
+                            &canvas->default_colors.colors [CHAFA_COLOR_PAIR_FG]);
+
+        for (i = 0; i < 3; i++)
+        {
+            differentiate_channel (&canvas->default_colors.colors [CHAFA_COLOR_PAIR_BG].ch [i],
+                                   canvas->default_colors.colors [CHAFA_COLOR_PAIR_FG].ch [i],
+                                   5);
+        }
+    }
 }
 
 static void

chafa/internal/chafa-private.h
@@ -109,6 +109,7 @@ struct ChafaCanvasConfig
     ChafaSymbolMap symbol_map;
     ChafaSymbolMap fill_symbol_map;
     guint preprocessing_enabled : 1;
+    guint hold_bg : 1;
     ChafaOptimizations optimizations;
 };
 

docs/chafa.xml
@@ -189,6 +189,15 @@ specified multiple times.
 </varlistentry>
 
 <varlistentry>
+<term><option>--hold-bg</option></term>
+<listitem><para>
+Leave the background color untouched. This produces character-cell output
+using foreground colors only, and will avoid resetting or inverting the
+colors.
+</para></listitem>
+</varlistentry>
+
+<varlistentry>
 <term><option>-h, --help</option></term>
 <listitem><para>
 Show a brief help text.

tools/chafa/chafa.c
@@ -72,6 +72,7 @@ typedef struct
     gboolean stretch;
     gboolean zoom;
     gboolean watch;
+    gboolean hold_bg;
     gint width, height;
     gint cell_width, cell_height;
     gdouble font_ratio;
@@ -250,6 +251,8 @@ print_summary (void)
     "                     yield much better quality but are rarely supported.\n"
     "      --glyph-file=FILE  Load glyph information from FILE, which can be any\n"
     "                     font file supported by FreeType (TTF, PCF, etc).\n"
+    "      --hold-bg      Leave the background color untouched. This produces\n"
+    "                     character-cell output using foreground colors only.\n"
     "      --invert       Invert video. For display with bright backgrounds in\n"
     "                     color modes 2 and none. Swaps --fg and --bg.\n"
     "  -O, --optimize=NUM  Compress the output by using control sequences\n"
@@ -856,6 +859,7 @@ parse_options (int *argc, char **argv [])
         { "format",      'f',  0, G_OPTION_ARG_CALLBACK, parse_format_arg,      "Format of output pixel data (ansi or sixels)", NULL },
         { "font-ratio",  '\0', 0, G_OPTION_ARG_CALLBACK, parse_font_ratio_arg,  "Font ratio", NULL },
         { "glyph-file",  '\0', 0, G_OPTION_ARG_CALLBACK, parse_glyph_file_arg,  "Glyph file", NULL },
+        { "hold-bg",     '\0', 0, G_OPTION_ARG_NONE,     &options.hold_bg,      "Hold background", NULL },
         { "invert",      '\0', 0, G_OPTION_ARG_NONE,     &options.invert,       "Invert foreground/background", NULL },
         { "optimize",    'O',  0, G_OPTION_ARG_INT,      &options.optimization_level,  "Optimization", NULL },
         { "preprocess",  'p',  0, G_OPTION_ARG_CALLBACK, parse_preprocess_arg,  "Preprocessing", NULL },
@@ -899,6 +903,7 @@ parse_options (int *argc, char **argv [])
     options.dither_grain_height = -1;  /* Unset */
     options.dither_intensity = 1.0;
     options.preprocess = TRUE;
+    options.hold_bg = FALSE;
     options.color_extractor = CHAFA_COLOR_EXTRACTOR_AVERAGE;
     options.color_space = CHAFA_COLOR_SPACE_RGB;
     options.width = 80;
@@ -1112,6 +1117,8 @@ build_string (ChafaPixelType pixel_type, const guint8 *pixels,
     chafa_canvas_config_set_fg_color (config, options.fg_color);
     chafa_canvas_config_set_bg_color (config, options.bg_color);
     chafa_canvas_config_set_preprocessing_enabled (config, options.preprocess);
+    chafa_canvas_config_set_hold_bg (config, options.hold_bg);
+
     if (options.transparency_threshold >= 0.0)
         chafa_canvas_config_set_transparency_threshold (config, options.transparency_threshold);
     if (options.cell_width > 0 && options.cell_height > 0)

commit 4d280ec2bb3e15b35cdd85d24f4e8eefa200001c

ChafaCanvasPrinter: Fix bad output caused by missing assignment
The error was introduced in c5ce9708cc2f9069cc6df7007b177464377ef0e7.

chafa/internal/chafa-canvas-printer.c
@@ -268,7 +268,7 @@ emit_attributes_256 (PrintCtx *ctx, gchar *out,
 {
     if (ctx->canvas->config.optimizations & CHAFA_OPTIMIZATION_REUSE_ATTRIBUTES)
     {
-        handle_inverted_with_reuse (ctx, out, fg, bg, inverted);
+        out = handle_inverted_with_reuse (ctx, out, fg, bg, inverted);
 
         if (fg != ctx->cur_fg)
         {
@@ -360,7 +360,7 @@ emit_attributes_16 (PrintCtx *ctx, gchar *out,
 {
     if (ctx->canvas->config.optimizations & CHAFA_OPTIMIZATION_REUSE_ATTRIBUTES)
     {
-        handle_inverted_with_reuse (ctx, out, fg, bg, inverted);
+        out = handle_inverted_with_reuse (ctx, out, fg, bg, inverted);
 
         if (fg != ctx->cur_fg)
         {