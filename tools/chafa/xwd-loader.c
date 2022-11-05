7f2a019a6d608c8d6539365c5bdfa9cdd293eeb2
Hans Petter Jansson <hpj@hpjansson.org>
2022-11-05 02:23:15 +0100

    XwdLoader: Remove input alignment requirement

--- tools/chafa/xwd-loader.c
+++ tools/chafa/xwd-loader.c
@@ -214,10 +214,6 @@ load_header (XwdLoader *loader)
     ASSERT_HEADER (h->header_size >= sizeof (XwdHeader));
     ASSERT_HEADER (h->header_size <= 65535);
 
-    /* FIXME: ImageMagick can generate 118-byte headers. Does Smolscale AVX2
-     * need multiples of 4 to avoid distortion, or is 2 ok? */
-    ASSERT_HEADER (h->header_size % 2 == 0);  /* For image data alignment */
-
     ASSERT_HEADER (h->file_version == 7);
     ASSERT_HEADER (h->pixmap_depth == 24);
 
