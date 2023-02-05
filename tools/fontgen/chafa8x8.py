126d3df864001e85e7e3a12056daed3609c8f383
Mo Zhou <cdluminate@gmail.com>
2023-02-04 21:59:33 -0500

    tools/fontgen: merge svg2ttf.py into chafa8x8.py

--- tools/fontgen/chafa8x8.py
+++ tools/fontgen/chafa8x8.py
@@ -5,7 +5,10 @@
 import os, sys
 import argparse
 import glob
-import json
+try:
+    import ujson as json
+except ModuleNotFoundError:
+    import json
 import time
 import random
 import numpy as np
@@ -218,13 +221,55 @@ def mainGenSVG(argv):
 
 
 def mainGenFont(argv):
-    call(['python3', 'svg2ttf.py'])
+    '''
+    Generate Font file
+    '''
+    ag = argparse.ArgumentParser()
+    ag.add_argument('--json', type=str, default='chafa8x8.json')
+    ag.add_argument('--ttf', type=str, default='chafa8x8.ttf')
+    ag = ag.parse_args(argv)
+    try:
+        import fontforge as ff
+    except ModuleNotFoundError:
+        print('You need to install the python3-fontforge package to generate the font')
+        exit(1)
+    import json
+
+    N = len(json.load(open('chafa8x8.json', 'r')))
+
+    try:
+        font = ff.font()
+        font.clear()
+        font.copyright = "(C) 2018 Mo Zhou <lumin@debian.org>, LGPLv3+"
+        font.fontname = "Chafa8x8"
+        font.familyname = "monospace"
+        font.fullname = "Chafa8x8 block glyphs by K-Means for character art"
+        font.version = "0a"
+        font.encoding = 'Custom'
+        print('Number of glyphs:', N)
+        for i in range(N):
+            glyph = font.createChar(0x100000 + i)
+            glyph.importOutlines('chafa8x8_svg/%d.svg'%i)
+            glyph.left_side_bearing = 0
+            glyph.right_side_bearing = 0
+            glyph.width = 0
+            glyph.vwidth = 0
+        #font.save('chafa8x8.sfd')
+        font.generate(ag.ttf)
+        font.close()
+    except Exception as e:
+        print(e)
+    print(f'done. the font has been written as {ag.ttf}')
 
 
 def mainGenA(argv):
+    print('calling ./chafa8x8.py Postproc')
     call(['./chafa8x8.py', 'Postproc'])
+    print('calling ./chafa8x8.py GenC')
     call(['./chafa8x8.py', 'GenC'])
+    print('calling ./chafa8x8.py GenSVG')
     call(['./chafa8x8.py', 'GenSVG'])
+    print('calling ./chafa8x8.py GenFont')
     call(['./chafa8x8.py', 'GenFont'])
 
 
