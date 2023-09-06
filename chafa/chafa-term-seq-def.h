66bfb9b25b0b2a96df62c90d8a2987d5e40777ea
Hans Petter Jansson <hpj@hpjansson.org>
2023-09-07 01:41:09 +0200

    seqs: Add tmux passthrough seqs

--- chafa/chafa-term-seq-def.h
+++ chafa/chafa-term-seq-def.h
@@ -2567,6 +2567,46 @@ CHAFA_TERM_SEQ_DEF(enable_alt_screen, ENABLE_ALT_SCREEN, 0, none, char)
  **/
 CHAFA_TERM_SEQ_DEF(disable_alt_screen, DISABLE_ALT_SCREEN, 0, none, char)
 
+/**
+ * chafa_term_info_emit_begin_tmux_passthrough:
+ * @term_info: A #ChafaTermInfo
+ * @dest: String destination
+ *
+ * Prints the control sequence for #CHAFA_TERM_SEQ_BEGIN_TMUX_PASSTHROUGH.
+ *
+ * @dest must have enough space to hold
+ * #CHAFA_TERM_SEQ_LENGTH_MAX bytes, even if the emitted sequence is
+ * shorter. The output will not be zero-terminated.
+ *
+ * Any control sequences between the beginning and end passthrough seqs
+ * must be escaped by turning \033 into \033\033.
+ *
+ * Returns: Pointer to first byte after emitted string
+ *
+ * Since: 1.14
+ **/
+CHAFA_TERM_SEQ_DEF(begin_tmux_passthrough, BEGIN_TMUX_PASSTHROUGH, 0, none, char)
+
+/**
+ * chafa_term_info_emit_end_tmux_passthrough:
+ * @term_info: A #ChafaTermInfo
+ * @dest: String destination
+ *
+ * Prints the control sequence for #CHAFA_TERM_SEQ_END_TMUX_PASSTHROUGH.
+ *
+ * @dest must have enough space to hold
+ * #CHAFA_TERM_SEQ_LENGTH_MAX bytes, even if the emitted sequence is
+ * shorter. The output will not be zero-terminated.
+ *
+ * Any control sequences between the beginning and end passthrough seqs
+ * must be escaped by turning \033 into \033\033.
+ *
+ * Returns: Pointer to first byte after emitted string
+ *
+ * Since: 1.14
+ **/
+CHAFA_TERM_SEQ_DEF(end_tmux_passthrough, END_TMUX_PASSTHROUGH, 0, none, char)
+
 #undef CHAFA_TERM_SEQ_AVAILABILITY
 
 #undef CHAFA_TERM_SEQ_ARGS
