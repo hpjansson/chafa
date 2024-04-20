0e7d5fbd576f579e196eb00a65b87f42c2a4517b
Hans Petter Jansson <hpj@hpjansson.org>
2024-04-20 18:11:47 +0200

    ChafaWorkCell: Add AVX2 implementation for mean color pair extraction
    It's about twice as fast as the MMX impl. in my tests. This
    makes a big difference with -w 9. The improvement is much
    smaller in the popcount-accelerated paths.

--- chafa/internal/chafa-work-cell.c
+++ chafa/internal/chafa-work-cell.c
@@ -83,7 +83,11 @@ chafa_work_cell_get_mean_colors_for_symbol (const ChafaWorkCell *wcell, const Ch
     const guint8 *covp = (guint8 *) &sym->coverage [0];
     ChafaColorAccum accums [2] = { 0 };
 
-#ifdef HAVE_MMX_INTRINSICS
+#ifdef HAVE_AVX2_INTRINSICS
+    if (chafa_have_avx2 ())
+        calc_colors_avx2 (wcell->pixels, accums, sym->mask_u32);
+    else
+#elif HAVE_MMX_INTRINSICS
     if (chafa_have_mmx ())
         calc_colors_mmx (wcell->pixels, accums, covp);
     else
