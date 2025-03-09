828284949a309d95de7a5a19940926108a93b8ff
Hans Petter Jansson <hpj@hpjansson.org>
2025-03-09 15:44:26 +0100

    tests: Test --grid argument

---
+++ tests/chafa-tool-grid-test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+[ "x${srcdir}" = "x" ] && srcdir="."
+. "${srcdir}/chafa-tool-test-common.sh"
+
+grid_sizes="1 4 x3 5x"
+
+for format in $formats; do
+    for size in $sizes; do
+        for grid_size in $grid_sizes; do
+            run_cmd_all_safe_files "$tool -f $format -c full -s $size --grid $grid_size --threads 12" || exit $?
+        done
+    done
+done
