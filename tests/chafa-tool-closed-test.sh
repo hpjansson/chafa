0d9fe5ea383424a1f2f6dfc0d1c6a430dc34e646
Hans Petter Jansson <hpj@hpjansson.org>
2025-06-29 01:24:49 +0200

    tests: Add a CLI test with closed stdin/stdout
    Closed fds can cause bugs, especially hangs in the I/O code.

---
+++ tests/chafa-tool-closed-test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Run with closed stdin/stdout/stderr and look for hangs.
+
+[ "x${srcdir}" = "x" ] && srcdir="."
+. "${srcdir}/chafa-tool-test-common.sh"
+
+run_cmd_noredir () {
+    cmd="$1 ${top_srcdir}/tests/data/good/taxic.jpg"
+    echo "$cmd" >&2
+    sh -c "$cmd" || exit $?
+}
+
+run_cmd_noredir "$timeout_cmd 10 $tool -f symbol -c full -s 200 <&- >/dev/null" || exit $?
+run_cmd_noredir "$timeout_cmd 10 $tool -f symbol -c full -s 200 >&- " || exit $?
+run_cmd_noredir "$timeout_cmd 10 $tool -f symbol -c full -s 200 <&- >&- " || exit $?
+run_cmd_noredir "$timeout_cmd 10 $tool -f symbol -c full -s 200 <&- >&- 2>&-" || exit $?
