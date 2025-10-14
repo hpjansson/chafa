37561c6455518c43448cb4cff949639bba4f25f3
Hans Petter Jansson <hpj@hpjansson.org>
2025-10-15 00:17:02 +0200

    tests: Limit memory use in grid test
    Don't be mean to CI environments.

--- tests/chafa-tool-grid-test.sh
+++ tests/chafa-tool-grid-test.sh
@@ -8,7 +8,7 @@ grid_sizes="1 4 x3 5x"
 for format in $formats; do
     for size in $sizes; do
         for grid_size in $grid_sizes; do
-            run_cmd_all_safe_files "$tool -f $format -c full -s $size --grid $grid_size --threads 12" || exit $?
+            run_cmd_all_safe_files "$tool -f $format -c full -s $size --grid $grid_size --threads 3" || exit $?
         done
     done
 done
