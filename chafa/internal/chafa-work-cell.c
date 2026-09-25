e890036adb4ff85c83cf0351531870cebdc76e84
Hans Petter Jansson <hpj@hpjansson.org>
2026-09-25 16:56:17 +0200

    ChafaWorkCell: Fix warning from older compilers
    Fully brace the initializer.

--- chafa/internal/chafa-work-cell.c
+++ chafa/internal/chafa-work-cell.c
@@ -78,7 +78,7 @@ chafa_work_cell_get_mean_colors_for_symbol (const ChafaWorkCell *wcell, const Ch
                                             ChafaColorPair *color_pair_out)
 {
     const guint8 *covp = (guint8 *) &sym->coverage [0];
-    ChafaColorAccum accums [2] = { 0 };
+    ChafaColorAccum accums [2] = { { { 0 } } };
 
 #ifdef HAVE_AVX2_INTRINSICS
     if (chafa_have_avx2 ())
